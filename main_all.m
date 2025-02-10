96105475b39b7a8589382969bffda02795d99519
Pawan Kumar <16705677+its-Pa1@users.noreply.github.com>
2025-02-10 12:46:45 +0100

    Add files via upload

---
+++ main_all.m
@@ -0,0 +1,15 @@
+clear all;
+close all;
+clc;
+
+% run('Full_model_I_II/main_for_Plots_set6E');
+% run('Full_model_I_II/main_for_Plots_set6H');
+run('Difference_minimal/main_diff_minimal_6F');
+run('Difference_minimal/main_diff_minimal_6F_II');
+run('Difference_self_diffusion/main_diff_self_d_6F');
+run('Pattern/Dim/Pattern_dim_main_6F');
+run('Pattern/Dim/Pattern_dim_set6F_I');
+run('Pattern_diff_W_L_flux/main_diff_W_L_flux_const');
+run('Pattern_diff_W_L_flux/main_diff_W_L_flux_I');
+run('Pattern_difference/Pattern_diff_const_main');
+run('Pattern_difference/Pattern_diff_I_main');
